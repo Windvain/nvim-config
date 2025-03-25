ee7e47d2d4d16e6cdff0c0b615be2b143eb0bf45
Windvain <rightscargames@gmail.com>
2025-03-25 21:33:09 +0700

    LSPs now working.. somewhat? (3/25/25)
    Also added .gitignore to remove lazy's json files.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,3 +28,52 @@ require("lazy").setup({
     },
     checker = { enabled = false },
 })
+
+-- If it's not broke don't fix it..
+local lspconf = require("lspconfig")
+local lspconf_defaults = lspconf.util.default_config
+
+lspconf_defaults.capabilities = vim.tbl_deep_extend(
+	"force",
+	lspconf_defaults.capabilities,
+	require("cmp_nvim_lsp").default_capabilities()
+)
+
+lspconf.cmake.setup({})
+lspconf.ccls.setup{
+	name = "ccls",
+
+	autostart = true,
+	single_file_support = false,	-- Enabling this makes all C++ projects with more than one file fail.
+	silent = false,
+
+	init_options = {
+		compilationDatabaseDirectory = "build"
+	},
+
+	root_dir = lspconf.util.root_pattern(
+		".git",
+		".editorconfig",
+		".clang-format"
+	)
+}
+
+local cmp = require("cmp")
+cmp.setup({
+	sources = {
+        { name = "nvim_lsp" }
+    },
+
+	snippet = {
+		expand = function(args)
+			vim.snippet.expand(args.body)
+		end
+	},
+
+	mapping = cmp.mapping.preset.insert({
+		["<Up>"] = cmp.mapping.select_prev_item(),
+		["<Down>"] = cmp.mapping.select_next_item(),
+		["<Esc>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true })
+	})
+})
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,51 +1,5 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = { "hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp" },
-
-	config = function()
-		local lspconf = require("lspconfig")
-		local lspconf_defaults = lspconf.util.default_config
-		local cmp = require("cmp")
-
-		lspconf_defaults.capabilities = vim.tbl_deep_extend(
-		    "force",
-		    lspconf_defaults.capabilities,
-		    require("cmp_nvim_lsp").default_capabilities()
-		)
-
-		lspconf.cmake.setup({})
-		lspconf.ccls.setup{
-			name = "ccls",
-
-			autostart = true,
-			single_file_support = false,	-- Enabling this makes all C++ projects with more than one file fail.
-			silent = false,
-
-			init_options = {
-				compilationDatabaseDirectory = "build"
-			},
-
-			root_dir = lspconf.util.root_pattern(
-				".git",
-				".editorconfig",
-				".clang-format"
-			)
-		}
-
-		cmp.setup({
-			sources = { name = "nvim_lsp" },
-			snippet = {
-				expand = function(args)
-					vim.snippet.expand(args.body)
-				end
-			},
-
-			mapping = cmp.mapping.preset.insert({
-				["<Up>"] = cmp.mapping.select_prev_item(),
-				["<Down>"] = cmp.mapping.select_next_item(),
-				["<Esc>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true })
-			})
-		})
-	end
+	{ "neovim/nvim-lspconfig" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/nvim-cmp" },
 }
